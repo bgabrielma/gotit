9459491459488fe243fc16e840cf497d03ae52dc
Bruno Martins <b.gabriel.ma.dev@gmail.com>
2026-04-29 10:36:58 +0100

    chore: add root TS, ESLint, Prettier, and Vitest configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/ban-ts-comment': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': ['error', { allow: ['warn', 'error'] }]
+  },
+  ignorePatterns: ['dist', 'build', 'node_modules', '*.cjs']
+}
